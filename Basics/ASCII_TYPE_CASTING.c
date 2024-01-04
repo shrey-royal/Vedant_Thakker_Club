6687fb2522dbb4bafaf4fed051cf94c06dda268d
Shrey K <srkkadia@gmail.com>
2024-01-04 19:31:39 +0530

    0

---
+++ Basics/ASCII_TYPE_CASTING.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main() {
+    //
+}
+
+/*
+ASCII: (Values)
+American Standard Code for Information Interchange.
+
+> made to be used to make all the computers portable and can support the data transfers from one another.
+
+Type casting: changing a datatype of a variable/value. 
+1. Implicit Type casting: it will be automatically
+> 
+
+
+2. Explicit Type casting: the user will have to force it.
+
+*/
