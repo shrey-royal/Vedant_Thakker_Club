26d57a6e6f7382bdb44ce6bbc53e513f645628fe
Shrey K <srkkadia@gmail.com>
2024-01-17 19:06:09 +0530

    Loops

---
+++ Control Statements/For.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int i;
+
+    // for(i=0; i<10; i++) {
+    //     printf("\ni = %d", i);
+    // }
+
+    // i=0;
+    // while(i<10) {
+    //     printf("\ni = %d", i);
+    //     i++;
+    // }
+
+    // i=0;
+    // do {
+    //     printf("\ni = %d", i);
+    //     i++;
+    // } while(i<10);
+}
+
+/*
+Loops: Used to execute same code multiple times until some condition is not satisfied.
+
+categories of loops: 
+    1. Entry Controlled Loops: a) for loop b) while loop
+    2. Exit Controlled Loops: c) do while loop
+
+syntaxes:
+
+for loop: used when we have fixed number of iterations
+
+for(intialization; condition; inc/dec) {
+    //body of the loop
+}
+
+while loop: used when we do not have fixed number of iterations
+
+intialization;
+while(condition) {
+    //body of the loop
+
+    //inc/dec
+}
+
+do while loop: used when we have to execute the body of the loop at least one time
+
+intialization;
+do {
+    //body of the loop
+
+    //inc/dec
+} while(condition);
+
+*/
