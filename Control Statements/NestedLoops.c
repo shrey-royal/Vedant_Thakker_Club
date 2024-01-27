e911fa038f3797192a70087cc1cafce2fc5e29c2
Shrey K <srkkadia@gmail.com>
2024-01-27 18:00:45 +0530

    Patterns

--- Control Statements/NestedLoops.c
+++ Control Statements/NestedLoops.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void main() {
-    int i, j, k, r, noOfSpaces=1;
+    // int i, j, k, r, noOfSpaces=1;
 
     // for(i=0; i<5; i++) {    //responsible for rows
     //     for(j=0; j<5; j++)    //responsible for cols
@@ -76,17 +76,49 @@ void main() {
 
     //Reverse Triangle
 
-    printf("\nEnter rows: ");
-    scanf("%d", &r);
+    // printf("\nEnter rows: ");
+    // scanf("%d", &r);
 
-    for(i=r; i>=1; i--) {
-        for(j=1; j<noOfSpaces; j++) {
-            printf("  ");
+    // for(i=r; i>=1; i--) {
+    //     for(j=1; j<noOfSpaces; j++) {
+    //         printf("  ");
+    //     }
+    //     for(k=1; k<=(2*i-1); k++) {
+    //         printf("* ");
+    //     }
+    //     noOfSpaces++;   //after every row the noOfSpaces will increase
+    //     printf("\n");
+    // }
+
+    //X like spaceX
+    //skeleton
+    // for(int i=1; i<8; i++) {    //responsible for rows
+    //     for(int j=1; j<8; j++)    //responsible for cols
+    //         printf("(%d, %d)\t", i, j);
+    //     printf("\n");
+    // }
+
+
+    int i, j, k, lines;
+
+    printf("\nEnter lines: ");
+    scanf("%d", &lines);
+
+    for (i=1; i<lines; i++) {
+        //first-half
+        for(j=1;j<=lines/2; j++) {
+            if(i == j) printf("%d ", j);
+            else if(i > (lines/2) && j == lines-i) printf("%d ", j);
+            else printf("  ");
         }
-        for(k=1; k<=(2*i-1); k++) {
-            printf("* ");
+        //second-half
+        // printf("\n\n\ni = %d\tj = %d\n\n", i, j);
+        // j-=2;   //j = j-2;
+        for(j-=2;j>0;j--) {
+            if(i == j) printf("%d ", j);
+            else if(i> (lines/2) && j == lines-i) printf("%d ", j);
+            else printf("  ");
         }
-        noOfSpaces++;   //after every row the noOfSpaces will increase
         printf("\n");
     }
 }
