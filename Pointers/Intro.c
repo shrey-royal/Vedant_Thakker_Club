57988059539702e0d5a5b58082fbebc46bcd8d78
Shrey K <srkkadia@gmail.com>
2024-02-14 18:58:05 +0530

    Pointer Basic

---
+++ Pointers/Intro.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main() {
+    //
+}
+
+/*
+Pointer: a variable that stores the address of another variable.
+a variable will have a asterisk(*) sign that is called an indirection operator.
+*/
