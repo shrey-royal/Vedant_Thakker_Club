abf6e6420aa5434ac056c54d9597432b755704ff
Shrey K <srkkadia@gmail.com>
2024-02-18 16:57:19 +0530

    Pointers introduction

--- Pointers/Intro.c
+++ Pointers/Intro.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
 void main() {
-    //
+    int var = 34;
+    int *ptr = &var;
+
+    printf("\nValue of var using var: %d", var);
+    printf("\nAddress of var using &var: %p", &var);
+    // printf("\nAddress of var using &var: %u", &var);
+    // printf("\nAddress of var using &var: %o", &var);
+    // printf("\nAddress of var using &var: %X", &var);
+    // printf("\nAddress of var using &var: %x", &var);
+
+    printf("\nvalue of var using *ptr: %d", *ptr);
+    printf("\nAddress of var using ptr: %p", ptr);
+    printf("\nAddress of ptr itself: %p", &ptr);
+
 }
 
 /*
